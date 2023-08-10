2a0b650e77bdadcf22012b3f6d0f84d1328ae48b
Camila <cf.silv466@gmail.com>
2023-08-10 17:00:01 -0300

    Componente Card criado.

--- nft-preview-card-component_novato/src/components/Main-content/index.jsx
+++ nft-preview-card-component_novato/src/components/Main-content/index.jsx
@@ -1,7 +1,10 @@
+import Card from "../Card/index";
+
 export default function MainContent() {
     return (
         <main className="main-content">
-           
+            
+            <Card />
         </main>
     )
 }
