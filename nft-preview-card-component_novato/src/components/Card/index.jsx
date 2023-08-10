427bf50717477c9292deb8f3bcc66a9a8a66e06d
Camila <cf.silv466@gmail.com>
2023-08-10 19:52:25 -0300

    Feita a estilização do card.

--- nft-preview-card-component_novato/src/components/Card/index.jsx
+++ nft-preview-card-component_novato/src/components/Card/index.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
+
 export default function Card() {
-    return (
-        <div>
-            
+  const [displayStyle, setDisplayStyle] = useState(false);
+
+  return (
+    <div className="card">
+      <div className="card__container-product-image">
+        <img
+          className="container-product-image__img"
+          src="public/image-equilibrium.jpg"
+          alt="/"
+          onMouseOver={() => setDisplayStyle(true)}
+        />
+        {displayStyle === true ? (
+          <div
+            className="container-product-image__icon-view"
+            onMouseOut={() => setDisplayStyle(false)}
+          >
+            <img src="public/icon/icon-view.svg" alt="" />
+          </div>
+        ) : null}
+      </div>
+      <div className="card__product-information">
+        <h1 className="product-information__title">Equilibrium #3429</h1>
+
+        <p className="product-information__paragraphy">
+          Our Equilibrium collection promotes <br /> balance and calm.
+        </p>
+
+        <div className="product-information__additional-information">
+          <span className="additional-information__container-value">
+            <img src="public/icon/icon-ethereum.svg" alt="" /> 0.041 eht
+          </span>
+          <span className="additional-information__day">
+            <img src="public/icon/icon-clock.svg" alt="/" /> 3 days left
+          </span>
         </div>
-    )
+
+        <div className="product-information__container-creator">
+          <img
+            className="container-creator__img-creator"
+            src="public/image-avatar.png"
+            alt="/"
+          />
+          <span className="container-creator__name">
+            <span className="name--color">Creation of </span> Jules Wyvern
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- nft-preview-card-component_novato/src/components/Main-content/index.jsx
+++ nft-preview-card-component_novato/src/components/Main-content/index.jsx
@@ -1,10 +1,9 @@
 import Card from "../Card/index";
 
 export default function MainContent() {
-    return (
-        <main className="main-content">
-            
-            <Card />
-        </main>
-    )
+  return (
+    <main className="main-content">
+      <Card />
+    </main>
+  );
 }
