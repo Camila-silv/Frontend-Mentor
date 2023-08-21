e5dbb258354a71ce8015ed09e21f05246e1203e9
Camila <cf.silv466@gmail.com>
2023-08-21 14:53:07 -0300

    Estilização do layout Main-content realizada.

--- stats-preview-card-component_novato/src/components/Main-content/index.jsx
+++ stats-preview-card-component_novato/src/components/Main-content/index.jsx
@@ -3,15 +3,15 @@ export default function MainContent() {
     <main className="main-content">
       <section className="main-content__container-card">
         <div className="container-card__card-header">
-            <img src="" alt="/"/>
+            <img src="public/image-header-mobile.jpg" alt="/" className="card-header__img"/>
         </div>
         <div className="container-card__card-body">
           <h1 className="card-body__title">
-            Get <span className="title--color">insights</span> that help your business grow.
+            Get <span className="title--color">insights</span> that help <br className="title--br"/> your business grow.
           </h1>
           <p className="card-body__paragraphy">
-            Discover the benefits of data analytics and make better decisions
-            regarding revenue, customer experience, and overall efficiency.{" "}
+            Discover the benefits of data analytics and  make <br className="paragraphy--br"/> better decisions
+            regarding revenue, customer <br className="paragraphy--br"/> experience, and overall efficiency.
           </p>
 
           <ul className="card-body__list-of-achievements">
